5894f007684226052882123425098ced35167cf6
ohya <ksk6348@gmail.com>
2018-07-22 20:03:41 +0900

    add reason

--- src/main/scala/jp/ynu/eis/forest/naga/analyzeSystem/QuestionAnalyzer.scala
+++ src/main/scala/jp/ynu/eis/forest/naga/analyzeSystem/QuestionAnalyzer.scala
@@ -8,7 +8,7 @@ case class QuestionAnalyzer(ur: UtteranceResult){
   def getResult: QuestionResult ={
     val analist = ur.questionList
     val gameInfo = ur.dm.gameInfoList
-    val questionClass = Map("role" -> false, "vote" -> false)
+    val questionClass = Map("role" -> false, "vote" -> false, "reason" -> false)
     if(ur.dm.taList.anaList.resc.wordData.head.contains(gameInfo.last.getAgent)){
       ur.dm.taList.anaList.resc.wordData.foreach(f => {
         if(f.contains("投票")){
@@ -17,7 +17,9 @@ case class QuestionAnalyzer(ur: UtteranceResult){
         if(f.contains("役職")){
           questionClass("role") = true
         }
-
+        if(f.contains("どうして")){
+          questionClass("reason") = true
+        }
       })
     }
 
